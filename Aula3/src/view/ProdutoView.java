74c4332f47c24a8ebcfa5e3c55dc4ce410065d56
Pedro Wernek <pedrowcnetto@gmail.com>
2026-03-31 21:29:26 -0300

    aula 5

---
+++ Aula3/src/view/ProdutoView.java
@@ -0,0 +1,27 @@
+package view;
+
+import model.Produto;
+
+import java.util.List;
+
+public class ProdutoView {
+
+    public void exibirProduto(Produto produto) {
+        System.out.println("\n=== Detalhes do Produto ===");
+        System.out.println("ID: " + produto.getId());
+        System.out.println("Nome: " + produto.getNome());
+        System.out.println("Preço: R$ " + produto.getPreco());
+    }
+
+    public void listarProdutos(List<Produto> produtos){
+        System.out.println("\n=== Lista de Produtos ===");
+        for (Produto p : produtos) {
+            System.out.println(p.getId() + " - " + p.getNome()
+                    + " - R$ " + p.getPreco());
+        }
+    }
+
+    public void mostrarMensagem(String msg) {
+        System.out.println(msg);
+    }
+}
